60dbb5e7a7e3a4eda9577aa62615e9c21723a82d
anupsawant <anup.sawant@northwestern.edu>
2017-04-21 23:53:47 -0500

    first commit

---
+++ ClozeCard.js
@@ -0,0 +1,27 @@
+function UserException(message) {
+   this.message = message;
+   this.name = 'UserException';
+}
+
+function ClozeCard(text,cloze){
+
+	if(this instanceof ClozeCard){
+		this.fullText = text;
+		this.cloze = cloze;
+		if(this.fullText.includes(this.cloze) === false){
+			throw "full text does not contain " + this.cloze;
+		}
+		this.partial = this.fullText.replace(this.cloze,"...");
+
+	}else{
+		return new ClozeCard(text,cloze);
+	}
+}
+s
+ClozeCard.prototype = {
+	displayCard : function(){
+		console.log(this.fullText,'----',this.cloze);
+	}
+}
+
+module.exports = ClozeCard;
